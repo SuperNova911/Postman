e06ec670e50e5229d056eb4396ded91fa6200ee8
Suwhan Kim <suwhan77@naver.com>
2020-04-22 15:50:52 +0900

    테스트 코드 업데이트

--- Postman/Program.cs
+++ Postman/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Postman
 {
@@ -6,7 +11,26 @@ namespace Postman
     {
         static void Main(string[] args)
         {
+            ActionSet();
+        }
 
+        private static void ActionSet()
+        {
+            DatabaseManager.Instance.Connect("Postman.db");
+
+            IEnumerable<Subscriber> subscribers = SubscribeManager.Instance.GetSubscribers();
+            if (subscribers.Count() == 0)
+            {
+                //return;
+            }
+
+            IEnumerable<string> addresses = subscribers.Select(x => x.Email);
+
+            string subject = $"[주가예측 알리미] {DateTime.Today.ToShortDateString()}";
+            string body = "Body test";
+
+            IMailSender mailSender = new GmailSender("", "", "Postman");
+            mailSender.SendMailAsync(new List<string> { "", "" }, subject, body).Wait();
 
             DatabaseManager.Instance.Close();
         }
