4d7b23eafb12b8ca252b559ccbd28c90534bcae7
Suwhan Kim <suwhan77@naver.com>
2020-05-04 19:15:46 +0900

    심플 로거

---
+++ Postman/Logger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Postman
+{
+    public class Logger
+    {
+        private static Logger instance;
+        private static readonly object instanceLock = new object();
+
+        private static readonly CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+
+        public Level LogLevel { get; set; }
+
+        private Logger()
+        {
+            LogLevel = Level.Trace;
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public enum Level
+        {
+            Trace, Debug, Info, Warn, Error, Fatal 
+        }
+
+        public void Log(Level level, string message, [CallerMemberName] string memberName = "")
+        {
+            if (LogLevel <= level)
+            {
+                Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH.mm.ss.fff}] {level} [{memberName}]: {message}");
+            }
+        }
+
+        public void Log(Level level, string message, Exception exception, [CallerMemberName] string memberName = "")
+        {
+            if (LogLevel <= level)
+            {
+                Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH.mm.ss.fff}] {level} [{memberName}]: {message}\n{exception}");
+            }
+        }
+    }
+}
